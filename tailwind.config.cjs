3900345d602d0b9916ea36f897bf8e22ead33061
mitsch01 <schwartz.miriam@gmail.com>
2025-10-07 14:57:54 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#f4c2c2' // example accent color
+      }
+    }
+  },
+  plugins: [],
+}
